b7c947196088372a4c2dbc8986bb5c24ed79349e
JuanPa <kgb_rnk@hotmail.com>
2013-07-08 10:10:05 -0500

    V 0.1

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'tienda_prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', 'hNi21uyiWYdMhIjkvn7G7OHxGOTHZJgbmEPqC9BQfeu8T9bUc5scOOC8');
+define('_COOKIE_IV_', '9qURSQiW');
+define('_PS_CREATION_DATE_', '2013-07-04');
+define('_PS_VERSION_', '1.5.4.1');
+define('_RIJNDAEL_KEY_', 'q7gCKqjkLQC606aT2EUqnIRGYHqmY9hq');
+define('_RIJNDAEL_IV_', 'XIqznMZzsoV0fkttT7IDhg==');
